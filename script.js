973eb60205ea34cea99650ce52fc680774494967
aderese98 <abrahamderese98@gmail.com>
2023-04-15 11:47:01 -0400

    Updated UI; Updated game functionality

--- script.js
+++ script.js
@@ -1,3 +1,86 @@
+let btns = document.querySelectorAll('#player-choices');
+let updates = document.querySelector('.console');
+let winnerUpdate = document.querySelector('.winner');
+let playerScore = 0, cpuScore = 0;
+
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        let val = e.target.innerHTML;
+        let playerChoice;
+        let compChoice = getComputerChoice().toUpperCase();
+        let playerWon = "";
+        let properVal = val == 'Rock' || val == 'Paper' || val == 'Scissors';
+        if(properVal){
+            playerChoice = val.toUpperCase();
+            updates.childNodes[1].innerHTML = "You chose: " + playerChoice;
+            updates.childNodes[3].innerHTML = "CPU chose: " + compChoice;
+        }
+        if(playerChoice == compChoice){
+            playerWon = "TIE";
+        }
+        if(playerChoice == 'ROCK'){
+            if(compChoice == 'PAPER'){
+                playerWon = "Computer";
+                cpuScore++;
+                updates.childNodes[5].innerHTML = "Score | You: " + playerScore +
+                " CPU: " + cpuScore;
+            }
+            else if(compChoice == 'SCISSORS'){
+                playerWon = "You";
+                playerScore++;
+                updates.childNodes[5].innerHTML = "Score | You: " + playerScore +
+                " CPU: " + cpuScore;
+            }
+        }
+        if(playerChoice == 'PAPER'){
+            if(compChoice == 'SCISSORS'){
+                playerWon = "Computer";
+                cpuScore++;
+                updates.childNodes[5].innerHTML = "Score | You: " + playerScore +
+                " CPU: " + cpuScore;
+            }
+            else if(compChoice == 'ROCK'){
+                playerWon = "You";
+                playerScore++;
+                updates.childNodes[5].innerHTML = "Score | You: " + playerScore +
+                " CPU: " + cpuScore;
+            }
+        }
+        if(playerChoice == 'SCISSORS'){
+            if(compChoice == 'ROCK'){
+                playerWon = "Computer";
+                cpuScore++;
+                updates.childNodes[5].innerHTML = "Score | You: " + playerScore +
+                " CPU: " + cpuScore;
+            }
+            else if(compChoice == 'PAPER'){
+                playerWon = "You";
+                playerScore++;
+                updates.childNodes[5].innerHTML = "Score | You: " + playerScore +
+                " CPU: " + cpuScore;
+            }
+        }
+        if(playerWon != "TIE" && properVal)
+            winnerUpdate.innerHTML = playerWon + " Won!";
+        else
+            winnerUpdate.innerHTML = playerWon;
+        
+        if(playerScore == 5 || cpuScore == 5) {
+            if(playerScore == 5)
+                winnerUpdate.innerHTML = "Congrats! " + playerWon + " won the game! Scores will be reset...";
+            else if(cpuScore == 5)
+                winnerUpdate.innerHTML = playerWon + " won the game. Better luck next time! Scores will be reset...";
+            playerScore = 0;
+            cpuScore = 0;
+            updates.childNodes[5].innerHTML = "Score | You: " + playerScore +
+                " CPU: " + cpuScore;
+            updates.childNodes[5].innerHTML = "Score | You: " + playerScore +
+                " CPU: " + cpuScore;
+        }
+        console.log(winnerUpdate.innerHTML);
+    })
+})
+
 function getComputerChoice(){
     let choices = ['Rock', 'Paper', 'Scissors'];
     let randomNum = Math.floor(Math.random() * 
@@ -6,48 +89,3 @@ function getComputerChoice(){
     return randomChoice;
 }
 
-function getPlayerChoice(){
-    return playerChoice;
-}
-
-function playGame(playerChoice, compChoice){
-    playerChoice = getPlayerChoice().toUpperCase();
-    compChoice = getComputerChoice().toUpperCase();
-    let playerWon = "";
-    if(playerChoice == compChoice){
-        alert('You both picked ' + playerChoice +
-              ", It's a Tie!");
-        playerWon = "TIE";
-    }
-    if(playerChoice == 'ROCK'){
-        if(compChoice == 'PAPER'){
-            alert('Computer chose Paper, You Lost!');
-            playerWon = "COMP";
-        }
-        else if(compChoice == 'SCISSORS'){
-            alert('Computer chose Scissors, You Won!');
-            playerWon = "PLAYER";
-        }
-    }
-    if(playerChoice == 'PAPER'){
-        if(compChoice == 'SCISSORS'){
-            alert('Computer chose Paper, You Lost!');
-            playerWon = "COMP";
-        }
-        else if(compChoice == 'ROCK'){
-            alert('Computer chose Scissors, You Won!');
-            playerWon = "PLAYER";
-        }
-    }
-    if(playerChoice == 'SCISSORS'){
-        if(compChoice == 'ROCK'){
-            alert('Computer chose Paper, You Lost!');
-            playerWon = "COMP";
-        }
-        else if(compChoice == 'PAPER'){
-            alert('Computer chose Scissors, You Won!');
-            playerWon = "PLAYER";
-        }
-    }
-    return playerWon;
-}
